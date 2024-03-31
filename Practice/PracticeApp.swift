857c089f6d519927711461af864859ef57df6cf5
mr-m0ke <ajglatczak@me.com>
2024-03-31 00:06:52 -0400

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Andrew Glatczak on 3/31/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
